42acd00a781d0a14f320f29f5c4414cf2a8699ec
zaidi mounib <mounibzaidi@gmail.com>
2023-09-25 23:11:41 +0100

    Assignment Completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,13 +3,26 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    setFoods((prevFoods) => [...prevFoods, newFood]);
   }
 
-  const foodList = foods.map((food) => (
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
+  const filteredFoods = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+
+  const foodList = filteredFoods.map((food) => (
     <li key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
@@ -18,6 +31,13 @@ function SpicyFoodList() {
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <ul>{foodList}</ul>
     </div>
   );
